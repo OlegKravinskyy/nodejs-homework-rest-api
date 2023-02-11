c2d008130acf213b55952a244c953df6d3bea533
OlegKravinskyy <kravinskyy@gmail.com>
2023-02-11 22:17:24 +0300

    finally changes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -106,25 +106,6 @@ class UsersController {
       throw error;
     }
   }
-
-  //   const { originalname, path: tmpDir } = req.file;
-  // const { _id } = req.user;
-
-  // try {
-  //   const [extension] = originalname.split(".").reverse();
-  //   const newImgName = `userAvatar_${_id}.${extension}`;
-  //   const originalImg = await Jimp.read(tmpDir);
-  //   const resizedImg = await originalImg.cover(250, 250);
-  //   await resizedImg.write(`${uploadDir}/avatars/${newImgName}`);
-  //   fs.unlink(tmpDir);
-  //   const avatar = path.join(avatarURLpath, newImgName);
-  //   const result = await auth.updateAvatar(avatar, _id);
-  //   const { avatarURL } = result;
-  //   res.status(200).json({ avatarURL });
-  // } catch (error) {
-  //   fs.unlink(tmpDir);
-  //   res.json({ error });
-  // }
 }
 
 module.exports = new UsersController();
